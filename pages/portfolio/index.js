1093c2714694815eedb201b1e8e3670a3ca6296c
Rupam Sutar <rupamsutar281998@gmail.com>
2023-10-26 06:03:51 +0530

    Working with nested Routes and paths

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+const Portfolio = () => {
+    return (
+        <div className="">
+            <h1>Portfolio Page</h1>
+        </div>
+    )
+}
+
+export default Portfolio;
---
+++ pages/portfolio/list.js
@@ -0,0 +1,9 @@
+const ListPage = () => {
+    return(
+        <div className="">
+            <h1>List Page</h1>
+        </div>
+    )
+}
+
+export default ListPage;
